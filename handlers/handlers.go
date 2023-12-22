6ce9bf7b0a8067208bef1c2e35ade3e1a3cb8094
prateek <prateekjoshi2013@gmail.com>
2023-12-21 19:33:10 -0700

    Added test session handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/prateekjoshi2013/scotch"
 )
 
@@ -28,5 +29,18 @@ func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h *Handlers) SessionTest(w http.ResponseWriter, r *http.Request) {
+	myData := "bar"
+	h.App.Session.Put(r.Context(), "foo", myData)
+	myValue := h.App.Session.Get(r.Context(), "foo")
+	vars := make(jet.VarMap)
+	vars.Set("foo", myValue)
+
+	err := h.App.Render.JetPage(w, r, "sessions", vars, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
 	}
 }
