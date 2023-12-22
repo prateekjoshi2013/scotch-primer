f467b06b8cd8fe1ff3e0e71ca35bfcf04e848d02
prateek <prateekjoshi2013@gmail.com>
2023-12-22 15:09:51 -0700

    Added testing postgres db connection

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,6 +15,19 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+	a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) {
+		query := "select id,first_name from public.users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+		var id int
+		var name string
+		err := row.Scan(&id, &name)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "id: %d, name: %s", id, name)
+
+	})
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
