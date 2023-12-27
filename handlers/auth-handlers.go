3d9789c51fa9d927181c82e5e12c5878199a97b4
prateek <prateekjoshi2013@gmail.com>
2023-12-27 15:48:21 -0700

    Added convinence methods

--- handlers/auth-handlers.go
+++ handlers/auth-handlers.go
@@ -3,7 +3,7 @@ package handlers
 import "net/http"
 
 func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
-	err := h.App.Render.Page(w, r, "login", nil, nil)
+	err := h.render(w, r, "login", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println(err)
 	}
--- routes.go
+++ routes.go
@@ -13,18 +13,18 @@ func (a *application) routes() *chi.Mux {
 	//middleware must come before routes
 
 	// add routes here
-	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.UserLogout)
-	a.App.Routes.Get("/form", a.Handlers.Form)
-	a.App.Routes.Post("/form", a.Handlers.SubmitForm)
+	a.get("/", a.Handlers.Home)
+	a.get("/go-page", a.Handlers.GoPage)
+	a.get("/jet-page", a.Handlers.JetPage)
+	a.get("/sessions", a.Handlers.SessionTest)
+	a.get("/users/login", a.Handlers.UserLogin)
+	a.post("/users/login", a.Handlers.PostUserLogin)
+	a.get("/users/logout", a.Handlers.UserLogout)
+	a.get("/form", a.Handlers.Form)
+	a.post("/form", a.Handlers.SubmitForm)
 
 	// testing user orm config
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "trevor",
 			LastName:  "Sawler",
@@ -39,7 +39,7 @@ func (a *application) routes() *chi.Mux {
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
 	})
 
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
 		users, err := a.Models.Users.GetAll()
 		if err != nil {
 			a.App.ErrorLog.Println(err)
@@ -49,7 +49,7 @@ func (a *application) routes() *chi.Mux {
 		}
 	})
 
-	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 		u, err := a.Models.Users.Get(id)
 		if err != nil {
@@ -60,7 +60,7 @@ func (a *application) routes() *chi.Mux {
 
 	})
 
-	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 		u, err := a.Models.Users.Get(id)
 		if err != nil {
